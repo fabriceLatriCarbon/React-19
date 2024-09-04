77b811299452ac7ee98b3afb77579fffe95edc8f
Fabrice Latri <fabrice.latri@hotmail.fr>
2024-09-04 23:39:06 +0200

    Added snapshot tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,19 +1,35 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from "./App"
 import { getAllTodos } from './features/todos/services/todo-service';
+import { Todo } from './features/todos/models/todo';
+
+const mockTodos: Todo[] = [
+  {
+    id: Date.now().toString(),
+    order: 1,
+    title: "First Test Todo",
+    completed: false
+  },
+  {
+    id: (Date.now() + 1).toString(),
+    order: 2,
+    title: "Second Test Todo",
+    completed: false
+  }
+]
 
 jest.mock('./features/todos/services/todo-service');
 
-const mockGetAllTodos = jest.mocked(getAllTodos).mockResolvedValue([]);
+const mockGetAllTodos = jest.mocked(getAllTodos);
 
 describe('App', () => {
 
   it('should render the app', async () => {
-    mockGetAllTodos.mockResolvedValue([]);
+    mockGetAllTodos.mockResolvedValue(mockTodos);
     
-    render(await App());
-  
-    await waitFor(() => expect(screen.getByText(/Write something/i)).toBeInTheDocument());
+    const { container } = render(await App());
+
+    expect(container).toMatchSnapshot();
   })
 })
 
